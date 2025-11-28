fb5adde40116677ab0f81332a8eb36c4ccf9058d
BekbolatBolebay <bolebay.bekbolat.25@gmail.com>
2025-11-28 17:07:22 +0500

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,5 +1,9 @@
-import { createBrowserClient } from "@supabase/ssr"
+// lib/supabase.ts
+import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
-  return createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
 }
